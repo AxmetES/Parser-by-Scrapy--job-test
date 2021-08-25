866252c910bd96e3f8fbdc9e93cff3c01524e829
AxmetES <fraktsia@gmail.com>
2021-08-25 23:58:55 +0600

    Add func . Add default var in .env file.

--- kolesa/kolesa/spiders/kolesa_spider.py
+++ kolesa/kolesa/spiders/kolesa_spider.py
@@ -10,19 +10,24 @@ env.read_env()
 
 class KolesaSpiderSpider(scrapy.Spider):
     name = 'kolesa_spider'
-    parts = urlparse(os.getenv('SITE_URL'))
+    parts = urlparse(os.getenv('SITE_URL', 'https://kolesa.kz/cars/vaz/nur-sultan/?auto-car-transm=2345'))
     main_url = parts.scheme + '://' + parts.netloc
 
-    start_urls = [os.getenv('SITE_URL')]
+    start_urls = [os.getenv('SITE_URL', 'https://kolesa.kz/cars/vaz/nur-sultan/?auto-car-transm=2345')]
+
+    def clean_price(self, text):
+        digits = [symbol for symbol in text if symbol.isdigit()]
+        clean_digits = ''.join(digits)
+        return clean_digits
 
     def parse(self, response):
         for car_div in response.css('div.a-elem'):
             title = car_div.css('a.ddl_product_link::text').get()
             price = car_div.css('span.price::text').get()
             link = urljoin(self.main_url, car_div.css('a.ddl_product_link').attrib['href'])
-
+            # (price.replace(u'\xa0', u' ')).strip('\n').strip()
             yield {'title': title,
-                   'price': (price.replace(u'\xa0', u' ')).strip('\n').strip(),
+                   'price': self.clean_price(price),
                    'link': link,
                    }
 
